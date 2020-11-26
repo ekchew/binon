4bf7ffe41e8613d2f2f4e2e938d342869463e5a8
ekchew <ekchew@gmail.com>
2020-11-25 19:13:37 -0500

    updated a comment

--- python36/binon/__init__.py
+++ python36/binon/__init__.py
@@ -4,7 +4,7 @@ This implementation of BinON requires Python 3.6 or later.
 
 from pathlib import Path
 
-#	Importing the ...Codec classes registers them to the BinON class.
+#	Importing the ...Obj classes registers them to the BinON class.
 py3Dir = Path(__file__).parent
 for path in py3Dir.iterdir():
 	modName = path.stem
