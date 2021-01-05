c455c408d1b0e5d25a57683776b547ba54e984f2
ekchew <ekchew@gmail.com>
2021-01-05 01:38:27 -0500

    added ListBase class

--- cpp17/headers/binon/listobj.hpp
+++ cpp17/headers/binon/listobj.hpp
@@ -7,29 +7,28 @@ namespace binon {
 
 	auto DeepCopyTList(const TList& list) -> TList;
 	void PrintTListRepr(const TList& list, std::ostream& stream);
-	
-	class ListObj:
-		public BinONObj,
-		public AccessContainer_mValue<ListObj,TList>
-	{
-	public:
+
+	struct ListBase: BinONObj {
+		virtual auto list() noexcept -> TList& = 0;
+		auto list() const noexcept -> const TList&
+			{ return const_cast<ListBase*>(this)->list(); }
+		template<typename Obj, typename... Args>
+			auto emplaceBack(Args&&... args) -> TSPBinONObj&;
+	};
+
+	struct ListObj: ListBase, AccessContainer_mValue<ListObj,TList> {
 		TValue mValue;
-		
+
 		ListObj(const TValue& v): mValue{v} {}
 		ListObj(TValue&& v) noexcept: mValue{std::move(v)} {}
 		ListObj() noexcept = default;
 		explicit operator bool() const noexcept override
 			{ return mValue.size() != 0; }
+		auto list() noexcept -> TList& final { return mValue; }
 		auto typeCode() const noexcept -> CodeByte final {return kListObjCode;}
-		template<typename Obj, typename... Args>
-			auto emplaceBack(Args&&... args) -> TSPBinONObj& {
-				mValue.push_back(
-					std::make_shared<Obj>(std::forward<Args>(args)...));
-				return mValue.back();
-			}
 		void encodeData(TOStream& stream, bool requireIO=true) const final;
 		void decodeData(TIStream& stream, bool requireIO=true) final;
-		
+
 		//	encodeElems() is like encodeData() except that it does not encode
 		//	the length of the list. It jumps straight to encoding the
 		//	elements. These methods, then, are useful if you can already tell
@@ -37,40 +36,31 @@ namespace binon {
 		void encodeElems(TOStream& stream, bool requireIO=true) const;
 		void decodeElems(TIStream& stream, TValue::size_type count,
 			bool requireIO=true);
-		
+
 		auto makeCopy(bool deep=false) const -> TSPBinONObj override;
 		auto clsName() const noexcept -> const char* override
 			{ return "ListObj"; }
 		void printArgsRepr(std::ostream& stream) const override
 			{ PrintTListRepr(mValue, stream); }
 	};
-	
+
 	struct SListVal {
 		CodeByte mElemCode = kIntObjCode;
 		TList mList;
 	};
-	class SList:
-		public BinONObj,
-		public AccessContainer_mValue<SList,SListVal>
-	{
-	public:
+	struct SList: ListBase,  AccessContainer_mValue<SList,SListVal> {
 		static constexpr bool kSkipAssertTypes = false;
-		
+
 		TValue mValue;
-		
+
 		SList(CodeByte elemCode) noexcept: mValue{elemCode} {}
 		SList(const TValue& v): mValue{v} {}
 		SList(TValue&& v) noexcept: mValue{std::move(v)} {}
 		SList() noexcept = default;
 		explicit operator bool() const noexcept override
 			{ return mValue.mList.size() != 0; }
+		auto list() noexcept -> TList& final { return mValue.mList; }
 		auto typeCode() const noexcept -> CodeByte final;
-		template<typename Obj, typename... Args>
-			auto emplaceBack(Args&&... args) -> TSPBinONObj& {
-				mValue.mList.push_back(
-					std::make_shared<Obj>(std::forward<Args>(args)...));
-				return mValue.mList.back();
-			}
 		void assertElemTypes() const;
 		void encodeData(TOStream& stream, bool requireIO=true) const final;
 		void decodeData(TIStream& stream, bool requireIO=true) final;
@@ -84,6 +74,15 @@ namespace binon {
 		void printArgsRepr(std::ostream& stream) const override;
 	};
 
+	//---- Template Implementation --------------------------------------------
+
+	template<typename Obj, typename... Args>
+	auto ListBase::emplaceBack(Args&&... args) -> TSPBinONObj& {
+		auto& lst = list();
+		lst.push_back(
+			std::make_shared<Obj>(std::forward<Args>(args)...));
+		return lst.back();
+	}
 }
 
 #endif
