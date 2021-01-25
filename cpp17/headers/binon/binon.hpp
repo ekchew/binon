b7b9187c419d3ce1ed5c0ee394fcfa0a37822354
ekchew <ekchew@gmail.com>
2021-01-25 12:23:22 -0500

    makefile/dependency improvements

--- cpp17/headers/binon/binon.hpp
+++ cpp17/headers/binon/binon.hpp
@@ -1,7 +1,9 @@
 #ifndef BINON_BINON_HPP
 #define BINON_BINON_HPP
 
-//	This header includes all the others.
+//	This header includes all the others. It is also touched if any of the others
+//	are altered and binon is rebuilt. That way, you can make this one header a
+//	dependency for any other projects that use libbinon.
 
 #include "binonobj.hpp"
 #include "boolobj.hpp"
