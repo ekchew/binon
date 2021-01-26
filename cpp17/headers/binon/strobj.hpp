71a967f45912cfb9efec777eb048512d2ee917eb
ekchew <ekchew@gmail.com>
2021-01-26 18:47:35 -0500

    working on C++ side

--- cpp17/headers/binon/strobj.hpp
+++ cpp17/headers/binon/strobj.hpp
@@ -15,7 +15,7 @@ namespace binon {
 		TValue mValue;
 
 		StrObj(const char* cStr): mValue{cStr} {}
-		StrObj(const std::string_view& sv): mValue{sv} {}
+		StrObj(const std::string_view& sv): mValue(sv.begin(), sv.end()) {}
 		StrObj(const TValue& v): mValue{v} {}
 		StrObj(TValue&& v) noexcept: mValue{std::move(v)} {}
 		StrObj() noexcept = default;
