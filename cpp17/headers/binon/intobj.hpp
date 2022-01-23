9c5cbec397a541e7000bcb0a09088e61da6d857f
Ted <ekchew@gmail.com>
2022-01-23 00:37:15 -0500

    IntVal and UIntVal to support big ints

--- cpp17/headers/binon/intobj.hpp
+++ cpp17/headers/binon/intobj.hpp
@@ -2,8 +2,132 @@
 #define BINON_INTOBJ_HPP
 
 #include "binonobj.hpp"
+#include "hystr.hpp"
+#include <type_traits>
+#include <variant>
 
 namespace binon {
+
+	//	IntBase is the parent of IntVal and UIntVal. The latter are used to
+	//	store integers encoded/decoded by BinON. The data structure is a
+	//	variant of an integral type and a byte vector. For IntVal (used by
+	//	IntObj), the integral type is std::int64_t. For UIntVal (used by
+	//	UIntObj), it is std::uint64_t.
+	//
+	//	The byte vector (std::vector<std::byte>) is typically used when you
+	//	have integers too big to fit into a 64-bit container. Such values get
+	//	stored in big-endian form in the vector.
+	//
+	//	This project does not provide any explicit support for 3rd party big
+	//	number libraries since it was a design goal not to require external
+	//	dependencies outside the standard library. But 2 methods are included
+	//	to encode/decode the integer data in hexadecimal string form (using
+	//	the FromHex() and asHex() methods, respectively). The GMP library, for
+	//	example, supports assigning hexadecimal strings to its objects.
+	template<typename Child, typename Fixed>
+		struct IntBase:
+			protected std::variant<Fixed, std::vector<std::byte>>
+		{
+			using TFixed = Fixed;
+			using TVariable = std::vector<std::byte>;
+
+			using std::variant<TFixed,TVariable>::variant;
+			using std::variant<TFixed,TVariable>::operator=;
+
+			auto isFixed() const noexcept -> bool;
+
+			//	These methods will throw std::bad_variant_access if you choose
+			//	the wrong one. Call isFixed() to check if you are unsure.
+			//
+			//	The asFixed() method (defined in subclasses) will narrow a
+			//	variable value if necessary rather than throw. The TFixed
+			//	conversion operator calls asFixed().
+			auto fixed() -> TFixed&;
+			auto fixed() const -> TFixed;
+			auto variable() -> TVariable&;
+			auto variable() const -> const TVariable&;
+			operator TFixed() const noexcept;
+
+			//	The as() method is equivalent to static_cast<T>(asFixed()).
+			//	It shouldn't throw in most cases unless T's constructor does.
+			//	If T is an integral type, you can call fits<T>() to check if
+			//	it has enough bits to accommodate the whole value.
+			template<typename T>
+				auto as() const -> T;
+		};
+	struct IntVal: IntBase<IntVal, std::int64_t> {
+
+		//	Reads an IntVal from a string containing hexadecimal digits.
+		//	Negative numbers are indicated by the most-significant bit of the
+		//	1st hex digit encountered. If you want to encode 255 rather than
+		//	-1, for example, you should write IntVal::FromHex("0ff") rather
+		//	than IntVal::FromHex("ff"). (Note that if you begin the string
+		//	with "0x", the '0' before the 'x' is ignored from the standpoint
+		//	of setting the sign. See the description under UIntVal for more
+		//	general info on this class method.
+		static auto FromHex(const HyStr& hex) -> IntVal;
+
+		//	asHex() is essentially the opposite of FromHex(), generating a
+		//	hexadecimal representation of the integer for you and returning it
+		//	in a std::string. It takes 2 optional arguments:
+		//		zerox: prepend a "0x" before the hexadecimal data?
+		//			This defaults to true.
+		//		wordSize: the number of bytes in a "word"
+		//			This parameter helps determine how many '0' or 'f' padding
+		//			digits should be inserted before the significant bits of
+		//			the integer begin. The hexadecimal representation should be
+		//			a multiple of wordSize bytes long.
+		//
+		//			Let's say the integer was 0x12345 and the wordSize was 8.
+		//			Then asHex should return "0x00012345". This is particularly
+		//			important when you are dealing with negative numbers, since
+		//			it will determine how many 'f' digits to insert for the
+		//			integer size you are considering to read negative.
+		//
+		//			The default 1 means asHex will insert at most 1 '0' or 'f'
+		//			to pad the number out to the nearest byte boundary.
+		auto asHex(
+			bool zerox = true, std::size_t wordSize = 1
+			) const -> std::string;
+
+		template<typename, typename> friend struct IntBase;
+		using IntBase<IntVal,TFixed>::IntBase;
+
+		auto asFixed() const noexcept -> TFixed;
+
+		//	Ahead of calling as() on an integral type, you can call fits<I>()
+		//	to check if the currently stored value can really fit inside your
+		//	type. For example, if the number is a million and you offer a
+		//	std::uint16_t, fits will return false.
+		//	Along somewhat similar lines is the byteCount() method, which
+		//	returns how many bytes would be needed to represent the current
+		//	integer. This can take a bit longer to calculate than fits().
+		template<typename Int>
+			auto fits() const noexcept -> bool;
+		auto byteCount() const noexcept -> std::size_t;
+	};
+	struct UIntVal: IntBase<UIntVal, std::uint64_t> {
+
+		//	Reads a UIntVal from a string containing hexadecimal digits.
+		//	Any non-hex characters are ignored (so if your string begins
+		//	with "0x", the 'x' should be skipped over as appropriate).
+		//	If there are no hex digits in the string, FromHex() will return
+		//	a UIntVal containing 0. Note that this method is smart enough to
+		//	use a TFixed representation if the number fits.
+		static auto FromHex(const HyStr& hex) -> UIntVal;
+		auto asHex(
+			bool zerox = true, std::size_t wordSize = 1
+			) const -> std::string;
+
+		template<typename, typename> friend struct IntBase;
+		using IntBase<UIntVal,TFixed>::IntBase;
+		auto asFixed() const noexcept -> TFixed;
+
+		template<typename UInt>
+			auto fits() const noexcept -> bool;
+		auto byteCount() const noexcept -> std::size_t;
+	};
+
 	struct TIntObj:
 		TStdAcc<TIntObj>,
 		TStdEq<TIntObj>,
@@ -95,6 +219,71 @@ namespace binon {
 	namespace types {
 		using UInt = UIntObj;
 	}
+
+	//==== Template Implementation =============================================
+
+	//---- IntBase -------------------------------------------------------------
+
+	template<typename Child, typename Fixed>
+		auto IntBase<Child,Fixed>::isFixed() const noexcept -> bool {
+			return std::holds_alternative<TFixed>(*this);
+		}
+	template<typename Child, typename Fixed>
+		auto IntBase<Child,Fixed>::fixed() -> TFixed& {
+			return std::get<TFixed>(*this);
+		}
+	template<typename Child, typename Fixed>
+		auto IntBase<Child,Fixed>::fixed() const -> TFixed {
+			return std::get<TFixed>(*this);
+		}
+	template<typename Child, typename Fixed>
+		auto IntBase<Child,Fixed>::variable() -> TVariable& {
+			return std::get<TVariable>(*this);
+		}
+	template<typename Child, typename Fixed>
+		auto IntBase<Child,Fixed>::variable() const -> const TVariable& {
+			return std::get<TVariable>(*this);
+		}
+	template<typename Child, typename Fixed>
+		IntBase<Child,Fixed>::operator TFixed() const noexcept {
+			return static_cast<const Child*>(this)->asFixed();
+		}
+	template<typename Child, typename Fixed> template<typename T>
+		auto IntBase<Child,Fixed>::as() const -> T {
+			return static_cast<T>(static_cast<const Child*>(this)->asFixed());
+		}
+
+	//---- IntVal --------------------------------------------------------------
+
+	template<typename Int>
+		auto IntVal::fits() const noexcept -> bool {
+			using std::get_if;
+			if(isFixed()) {
+				if(sizeof(Int) >= sizeof(TFixed)) {
+					return true;
+				}
+				auto limit = 1ULL << (sizeof(Int) * 8 - 1);
+				auto i = *get_if<TFixed>(this);
+				return -limit <= i && i < limit;
+			}
+			return get_if<TVariable>(this)->size() <= sizeof(Int);
+		}
+
+	//---- UIntVal -------------------------------------------------------------
+
+	template<typename UInt>
+		auto UIntVal::fits() const noexcept -> bool {
+			using std::get_if;
+			if(isFixed()) {
+				if(sizeof(UInt) >= sizeof(TFixed)) {
+					return true;
+				}
+				auto limit = 1ULL << (sizeof(UInt) * 8);
+				auto i = *get_if<TFixed>(this);
+				return i < limit;
+			}
+			return get_if<TVariable>(this)->size() <= sizeof(UInt);
+		}
 }
 
 namespace std {
--- cpp17/source/intobj.cpp
+++ cpp17/source/intobj.cpp
@@ -1,6 +1,254 @@
 #include "binon/intobj.hpp"
 
+#include <algorithm>
+#include <cctype>
+#include <iomanip>
+#include <sstream>
+
 namespace binon {
+
+	//---- Local functions -----------------------------------------------------
+
+	static auto FilterHex(const HyStr& hex) {
+		auto iter = hex.begin();
+		for(; iter != hex.end(); ++iter) {
+			if(std::tolower(*iter) == 'x') {
+				break;
+			}
+		}
+		if(iter == hex.end()) {
+			iter = hex.begin();
+		}
+		return [&hex, iter]() mutable -> char {
+			for(; iter != hex.end(); ++iter) {
+				if(std::isxdigit(*iter)) {
+					return *iter++;
+				}
+			}
+			return '\xff';
+		};
+	}
+	static auto CountHexDigits(const HyStr& hex) {
+		std::size_t n = 0;
+		auto filter = FilterHex(hex);
+		while(filter() != '\xff') {
+			++n;
+		}
+		return n;
+	}
+	static auto HexCodeGen(const HyStr& hex, std::stringstream& ss) {
+		ss << std::hex;
+		auto filter = FilterHex(hex);
+		return [&ss, filter]() mutable -> std::byte {
+			auto c = filter();
+			if(c == '\xff') {
+				return 0xff_byte;
+			}
+			ss.seekp(0);
+			ss.seekg(0);
+			ss << c;
+			int digit;
+			ss >> digit;
+			return ToByte(digit & 0xf);
+		};
+	}
+	static auto BytesFromHex(const HyStr& hex, std::size_t nDigits) {
+		std::stringstream ss;
+		auto gen = HexCodeGen(hex, ss);
+		std::vector<std::byte> v;
+		v.reserve((nDigits + 1) >> 1);
+		bool flush = (nDigits & 0x1) != 0x0;
+		std::byte byt = 0x00_byte;
+		auto digit = gen();
+		while(digit != 0xff_byte) {
+			byt <<= 4;
+			byt |= digit;
+			if(flush) {
+				v.push_back(byt);
+				byt = 0x00_byte;
+			}
+			flush = !flush;
+			digit = gen();
+		}
+		return v;
+	}
+
+	//---- IntVal --------------------------------------------------------------
+
+	auto IntVal::FromHex(const HyStr& hex) -> IntVal {
+		auto n = CountHexDigits(hex);
+		auto bytes{BytesFromHex(hex, n)};
+		if((n & 0x1) != 0x0 && (bytes[0] & 0x08_byte) != 0x00_byte) {
+			bytes[0] |= 0xf0_byte;
+		}
+		if(n > sizeof(TFixed) << 1) {
+			return bytes;
+		}
+		return IntVal{bytes}.asFixed();
+	}
+	auto IntVal::asHex(
+		bool zerox, std::size_t wordSize
+		) const -> std::string
+	{
+		using std::get_if;
+		using std::to_integer;
+		using std::setfill;
+		using std::setw;
+		std::ostringstream oss;
+		oss << std::hex;
+		if(zerox) {
+			oss << "0x";
+		}
+		auto n = byteCount();
+		auto m = n % wordSize;
+		if(m > 0u) {
+			m = wordSize - m;
+		}
+		if(isFixed()) {
+			auto i = *get_if<TFixed>(this);
+			if(m > 0u) {
+				const char* s = i < 0 ? "ff" : "00";
+				while(m-->0) {
+					oss << s;
+				}
+			}
+			oss << setfill('0');
+			while(n-->0) {
+				oss << setw(2) << ((i >> (n << 3)) & 0xff);
+			}
+		}
+		else {
+			auto& v = *get_if<TVariable>(this);
+			if(m > 0u) {
+				bool neg = v.size() > 0 && (v[0] & 0x80_byte) != 0x00_byte;
+				oss << setfill(neg ? 'f' : '0');
+				while(m-->0) {
+					oss << setw(2) << 0;
+				}
+			}
+			oss << setfill('0');
+			for(auto b: v) {
+				oss << setw(2) << to_integer<unsigned>(b);
+			}
+		}
+		return oss.str();
+	}
+	auto IntVal::asFixed() const noexcept -> TFixed {
+		using std::get_if;
+		if(isFixed()) {
+			return *get_if<TFixed>(this);
+		}
+		TFixed i = 0;
+		bool first = true;
+		int j = 0;
+		for(auto byt: *get_if<TVariable>(this)) {
+			if(first) {
+				if((byt & 0x80_byte) != 0x00_byte) {
+					i = -1;
+				}
+				first = false;
+			}
+			i <<= 8;
+			i |= std::to_integer<TFixed>(byt) & 0xff;
+		}
+		return i;
+	}
+	auto IntVal::byteCount() const noexcept -> std::size_t {
+		using std::get_if;
+		using std::size_t;
+		if(isFixed()) {
+			auto i = *get_if<TFixed>(this);
+			std::size_t n = 0;
+			int shift = 7;
+			if(i < 0) {
+				while(i != -1) {
+					std::cerr << i << '\n';
+					i >>= shift;
+					shift = 8;
+					++n;
+				}
+			}
+			else {
+				while(i != 0) {
+					i >>= shift;
+					shift = 8;
+					++n;
+				}
+			}
+			return std::min<size_t>(std::max<size_t>(n, 1), 8);
+		}
+		return get_if<TVariable>(this)->size();
+	}
+
+	//---- UIntVal -------------------------------------------------------------
+
+	auto UIntVal::FromHex(const HyStr& hex) -> UIntVal {
+		auto n = CountHexDigits(hex);
+		UIntVal i{BytesFromHex(hex, n)};
+		if(n <= sizeof(TFixed) << 1) {
+			i = i.asFixed();
+		}
+		return i;
+	}
+	auto UIntVal::asHex(
+		bool zerox, std::size_t wordSize
+		) const -> std::string
+	{
+		using std::get_if;
+		using std::to_integer;
+		using std::setw;
+		std::ostringstream oss;
+		oss << std::hex << std::setfill('0');
+		if(zerox) {
+			oss << "0x";
+		}
+		auto n = byteCount();
+		auto m = n % wordSize;
+		if(m > 0u) {
+			m = wordSize - m;
+			while(m-->0) {
+				oss << "00";
+			}
+		}
+		if(isFixed()) {
+			auto i = *get_if<TFixed>(this);
+			while(n-->0) {
+				oss << setw(2) << ((i >> (n << 3)) & 0xff);
+			}
+		}
+		else {
+			auto& v = *get_if<TVariable>(this);
+			for(auto b: v) {
+				oss << setw(2) << to_integer<unsigned>(b);
+			}
+		}
+		return oss.str();
+	}
+	auto UIntVal::asFixed() const noexcept -> TFixed {
+		using std::get_if;
+		if(isFixed()) {
+			return *get_if<TFixed>(this);
+		}
+		TFixed i = 0;
+		for(auto byt: *get_if<TVariable>(this)) {
+			i <<= 8;
+			i |= std::to_integer<TFixed>(byt) & 0xff;
+		}
+		return i;
+	}
+	auto UIntVal::byteCount() const noexcept -> std::size_t {
+		using std::get_if;
+		if(isFixed()) {
+			auto i = *get_if<TFixed>(this);
+			std::size_t n = 0;
+			while(i != 0) {
+				i >>= 8;
+				++n;
+			}
+			return std::max<std::size_t>(n, 1);
+		}
+		return get_if<TVariable>(this)->size();
+	}
 
 	//---- TIntObj -------------------------------------------------------------
 
