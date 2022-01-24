f60dcfcdd417472a28aeeab7bcf2991b2422296a
Ted <ekchew@gmail.com>
2022-01-24 00:50:00 -0500

    added back some documentation

--- cpp17/headers/binon/intobj.hpp
+++ cpp17/headers/binon/intobj.hpp
@@ -9,31 +9,117 @@
 
 namespace binon {
 
+	/*
+	Class hierarchy:
+		std::variant
+			IntBase:
+				IntVal
+				UIntVal
+
+	IntVal and UIntVal store the integer values used by TIntObj and TUIntObj,
+	respectively. They do so in a variant record containing either a 64-bit
+	C++ integer type or a byte buffer of arbitrary length for really big
+	numbers. In the latter case, the bytes are ordered big-endian.
+	*/
 	template<typename Child, typename Scalar>
 		struct IntBase: std::variant<Scalar, std::basic_string<std::byte>>
 		{
+			/*
+			IntBase exposes all the functionality (including constructors)
+			of the std::variant it inherits from. It also has full access its
+			child classes--IntVal and UIntVal--through the CRTP paradigm.
+
+			The variant's Scalar type is supplied by the child class in the
+			2nd template argument, but is std::int64_t for IntVal and
+			std::uint64_t for UIntVal.
+
+			The vector type is actually a string of std::byte. (It used to be
+			a std::vector<std::byte> but was changed to string due to several
+			advantages the latter boasts, like the small string optimization
+			and built-in hashing support.)
+			*/
 			using TScalar = Scalar;
 			using TVect = std::basic_string<std::byte>;
 
 			using std::variant<TScalar,TVect>::variant;
 
+			/*
+			isScalar() vs. canBeScalar()
+
+			The isScalar() method is a quick way to tell whether the variant
+			current holds a scalar or a vector.
+
+			canBeScalar() may return true even in the vector case if the vector
+			is short enough to be converted into a 64-bit integer without any
+			truncation. It is not a bad idea to call it before calling scalar().
+			*/
 			auto isScalar() const noexcept -> bool;
 			auto canBeScalar() const noexcept -> bool;
 
+			/*
+			Scalar access/conversion
+
+			If the variant already holds a scalar, the scalar() method will
+			return it right away. Otherwise, it will check if the vector is
+			short enough to convert to a 64-bit integer without truncsation.
+			If so, scalar() will perform the conversion in-place and return it.
+			(Note that this will happen even with the const version of the
+			method.) Otherwise, it will throw std::bad_variant_access.
+
+			scalar() is also mapped onto an implicit conversion operator to
+			TScalar. This is to help IntVal and UIntVal to function in many
+			situations where you would want to work with a std::int64_t or
+			std::uint64_t directly, but be aware of this potential for an
+			exception.
+
+			The asScalar() method (defined in the child classes) will return
+			the 64-bit integer without throwing any exceptions. In other words,
+			it will truncate if necessary without complaining. There is also
+			an asNum<N>() method that casts to any numeric type. asNum<int>(),
+			for example, is equivalent to static_cast<int>(asScalar()).
+			(asNum<N>() will only throw an exception if N is some weird class
+			whose constructor could throw.)
+			*/
 			auto scalar() -> TScalar&;
 			auto scalar() const -> TScalar;
+			operator TScalar() const;
+			template<typename N>
+				auto asNum() const -> N;
+
+			/*
+			Vector access/conversion
+
+			vect() is the counterpart to scalar() in that it returns the
+			vector variant where available. It will NOT, however, perform an
+			in-place conversion from scalar but throw std::bad_variant_access
+			instead.
+
+			asVect() will perform the conversion though, throwing an exception
+			only if it runs out of memory in allocating the vector. This method
+			comes in two forms. The simpler is much like asScalar(). It simply
+			returns the vector by value. The lower-level form accepts a
+			callback functor you would typically implement using a lambda
+			expression. The callback accespts a single argument--a constant
+			reference to the vector--and may return a type you supply. This
+			will then get returned by the asVect() method itself. The advantage
+			of the callback approach is that it can be a bit more efficient
+			when the variant already contains a vector, since it can pass a
+			reference to it rather than returning a copy.
+			*/
 			auto vect() -> TVect&;
 			auto vect() const -> const TVect&;
-			operator TScalar() const;
 			auto asVect() const -> TVect;
 			template<typename ReturnType>
 				auto asVect(
 					std::function<ReturnType(const TVect&)> callback
 					) const -> ReturnType;
 
-			template<typename T>
-				auto as() const -> T;
 		};
+
+	/*
+	When printing an integer value, it will either print the scalar or a
+	hexadecimal version of the vector, depending on which variant is available.
+	*/
 	template<typename Child, typename Scalar>
 		auto operator<< (
 			std::ostream& stream, const IntBase<Child,Scalar>& i
@@ -41,10 +127,35 @@ namespace binon {
 
 	struct IntVal: IntBase<IntVal, std::int64_t> {
 
+		/*
+		You can convert both IntVal and UIntVal into a hexadecimal string by
+		calling asHex(), or build either object by calling the FromHex()
+		class method.
+
+		FromHex() accepts a HyStr--or hybrid string--which can be either a
+		std::string or a std::string_view (see hystr.hpp). The hexadecimal may
+		or may not begin with a "0x". FromHex() does not care.
+		Any non-hexadecimal characters are ignored. Both lower and upper case
+		are fine for any letter digits.
+
+		asHex()'s first argument--zerox--determines whether it should insert
+		"0x" at the beginning of the string it returns. All output is lower case
+		with no non-hexadecimal characters (beyond the 0x).
+
+		Both methods have an optional second argument: the word size. This has
+		a bearing on how many pad bytes (if any) to insert ahead of the data.
+		The default 16 (the length of a 64-bit scalar) means a number like
+		0x123abc will be printed "0x0000000000123abc". The pad bytes can be
+		especially important for IntVal which support negative numbers. In this
+		case, they will be "ff" rather than "00". If the number of significant
+		bytes exceeds the word size, the padding will be extended to the next
+		word boundary. For example, if your integer were 0x12345 and your
+		word size was only 4, you would get 0x00012345 (in other words, 2
+		4-byte words).
+		*/
 		static auto FromHex(const HyStr& hex,
 			std::size_t wordSize = sizeof(TScalar)
 			) -> IntVal;
-
 		auto asHex(
 			bool zerox = true, std::size_t wordSize = sizeof(TScalar)
 			) const -> std::string;
@@ -54,6 +165,11 @@ namespace binon {
 
 		auto asScalar() const noexcept -> TScalar;
 
+		/*
+		The fits() method determines whether the current integer would fit an
+		integer type you specify without truncation. It can be useful to call
+		fits<int>() before asNum<int>(), for example.
+		*/
 		template<typename Int>
 			auto fits() const noexcept -> bool;
 	};
@@ -209,9 +325,9 @@ namespace binon {
 		IntBase<Child,Scalar>::operator TScalar() const {
 			return static_cast<const Child*>(this)->scalar();
 		}
-	template<typename Child, typename Scalar> template<typename T>
-		auto IntBase<Child,Scalar>::as() const -> T {
-			return static_cast<T>(static_cast<const Child*>(this)->asScalar());
+	template<typename Child, typename Scalar> template<typename N>
+		auto IntBase<Child,Scalar>::asNum() const -> N {
+			return static_cast<N>(static_cast<const Child*>(this)->asScalar());
 		}
 	template<typename Child, typename Scalar>
 		auto IntBase<Child,Scalar>::asVect() const -> TVect {
