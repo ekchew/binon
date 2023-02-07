d3c46ee95accb8e7186624e7a4cfe5611f187738
Ted <ekchew@gmail.com>
2023-02-07 02:00:24 -0500

    hashutil functions adapted from master branch

--- cpp17/source/hashutil.cpp
+++ cpp17/source/hashutil.cpp
@@ -32,25 +32,4 @@ namespace binon {
 		}
 
 	}();
-
-	void CommutativeHash::extend(std::size_t hashVal) {
-		mHash ^= (hashVal ^ (hashVal << 16) ^ 89869747UL) * 3644798167UL;
-	}
-	auto CommutativeHash::get() const -> std::size_t {
-		auto h = mHash;
-		h = h * 69069U + 907133923UL;
-
-		//	I think this final step in the frozenset hash is only necessary
-		//	because their implementation uses -1 as a flag to indicate whether
-		//	or not a hash has already been calculated? So I am omitting it here
-		//	but leaving it commented out in case there is more to it than I am
-		//	aware of.
-	 #if 0
-		if(h == -1) {
-			h = 590923713UL;
-		}
-	 #endif
-
-		return h;
-	}
 }
