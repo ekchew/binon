290ff30d73c4d1681923f47f544f9f29616c8e25
ekchew <ekchew@gmail.com>
2023-02-14 18:26:10 -0500

    updating main branch with improved serialization code

--- cpp17/headers/binon/byteutil.hpp
+++ cpp17/headers/binon/byteutil.hpp
@@ -2,6 +2,7 @@
 #define BINON_BYTEUTIL_HPP
 
 #include "errors.hpp"
+#include "floattypes.hpp"
 #include "ioutil.hpp"
 
 #include <algorithm>
@@ -9,6 +10,7 @@
 #include <climits>
 #include <cstddef>
 #include <cstring>
+#include <iterator>
 #include <optional>
 #include <limits>
 #include <stdexcept>
@@ -152,6 +154,312 @@ namespace binon {
  #else
 	auto LittleEndian() noexcept -> bool;
  #endif
+
+	//-------------------------------------------------------------------------
+	//
+	//	Byte-Like Type Handling
+	//
+	//	Byte-like types include std::byte as well as any single-byte integral
+	//	types such as char.
+	//
+	//-------------------------------------------------------------------------
+
+	//	ConvByteLike function:
+	//		Converts a value from one byte-like type to another.
+	template<typename To, typename From> constexpr
+		auto ConvByteLike(From v) noexcept -> To {
+			static_assert(sizeof(From) == 1U and sizeof(To) == 1U);
+			using std::byte, std::is_same_v;
+			if constexpr(is_same_v<From, byte>) {
+				if constexpr(is_same_v<To, byte>) {
+					return v;
+				}
+				else {
+					return std::to_integer<To>(v);
+				}
+			}
+			else
+				if constexpr(is_same_v<To, byte>) {
+					return static_cast<std::underlying_type_t<byte>>(v);
+				}
+				else {
+					return static_cast<To>(v);
+				}
+		}
+
+	//-------------------------------------------------------------------------
+	//
+	//	Serialization
+	//
+	//	The functions defined here help serialize fundamental data types
+	//	(integral or floating-point) into byte sequences and vice versa.
+	//
+	//	For example, you could encode the integer 1000 into 2 bytes with:
+	//
+	//		auto arr = BytePack<2>(1000);
+	//
+	//	Here, arr will be a std::array<TStreamByte,2> containing:
+	//
+	//		{'\x03','\xf8'}
+	//
+	//	As you can see, serialization follows a big-endian byte ordering
+	//	convention.
+	//
+	//	Later, you can unpack the serialized bytes with:
+	//
+	//		auto i = ByteUnpack<int,2>(arr);  // i = 1000
+	//
+	//	There are also WriteAsBytes() and ReadAsBytes() functions that
+	//	serialize data to/from TOStream and TIStream streams, respectively.
+	//
+	//	Regarding floating-point values, BinON currently only supports IEEE 754
+	//	binary32 and binary64 formats, so the size needs to be either 4 or 8
+	//	bytes. binon::types::TFloat32 and binon::types::TFloat64 are meant to
+	//	map onto these two data types.
+	//
+	//-------------------------------------------------------------------------
+
+	//	kBytePackSize constant:
+	//		This evaluates how large the std::array returned by BytePack()
+	//		should be dimensioned.
+	template<typename T, std::size_t Size>
+		inline constexpr std::size_t kBytePackSize = Size ? Size : sizeof(T);
+
+	namespace details {
+		template<std::size_t Size=0, typename Byte=TStreamByte, typename Int>
+			auto BytePackInt(Int i) noexcept
+				-> std::array<Byte, kBytePackSize<Int, Size>>
+			{
+				static_assert(std::is_integral_v<Int>);
+				using std::array;
+				using UC = unsigned char;
+				constexpr std::size_t N = kBytePackSize<Int, Size>;
+				if constexpr(N == 1U) {
+					return std::array<Byte, N>{ConvByteLike<Byte>(i)};
+				}
+				else {
+					std::array<Byte, N> arr;
+					auto end = arr.rend();
+					for(auto it = arr.rbegin(); it != end; ++it, i >>= 8) {
+						*it = ConvByteLike<Byte>(static_cast<UC>(i & 0xffU));
+					}
+					return arr;
+				}
+			}
+
+		template<std::size_t Size=0, typename Byte=TStreamByte, typename Flt>
+			auto BytePackFloat(Flt x) noexcept
+				-> std::array<Byte, kBytePackSize<Flt, Size>>
+			{
+				static_assert(std::is_floating_point_v<Flt>);
+				using std::array;
+				constexpr std::size_t N = kBytePackSize<Flt, Size>;
+				static_assert(N == 4 or N == 8,
+					"BinON expects binary32 or binary64 floating-point types");
+				auto pack = [](auto x) noexcept -> array<Byte, sizeof x> {
+					array<Byte, sizeof x> arr;
+					std::memcpy(arr.data(), &x, sizeof x);
+					if(LittleEndian()) {
+						std::reverse(arr.begin(), arr.end());
+					}
+					return arr;
+				};
+				if constexpr(N == 4) {
+					return pack(static_cast<types::TFloat32>(x));
+				}
+				else {
+					return pack(static_cast<types::TFloat64>(x));
+				}
+			}
+
+		template<
+			typename UInt, std::size_t Size=sizeof(UInt),
+			typename Byte=TStreamByte
+			>
+			constexpr auto ByteUnpackUInt(const std::array<Byte, Size>& arr)
+				noexcept -> UInt
+			{
+				static_assert(std::is_unsigned_v<UInt>);
+				using UC = unsigned char;
+				UInt i{};
+				for(auto b: arr) {
+					i <<= 8;
+					i |= ConvByteLike<Byte>(static_cast<UC>(b & 0xffU));
+				}
+				return i;
+			}
+
+		template<
+			typename SInt, std::size_t Size=sizeof(SInt),
+			typename Byte=TStreamByte
+			>
+			auto ByteUnpackSInt(const std::array<Byte, Size>& arr) noexcept
+				-> SInt
+			{
+				static_assert(
+					std::is_integral_v<SInt> and not std::is_unsigned_v<SInt>);
+				using UInt = std::make_unsigned_t<SInt>;
+				UInt u = ByteUnpackUInt(arr);
+				if constexpr(Size >= sizeof(SInt)) {
+					return static_cast<SInt>(u);
+				}
+				else {
+					UInt m = UInt{1} << (Size * 8U - 1U);
+					SInt v = static_cast<SInt>(u & (m - 1U));
+					if(u & m) {
+						v -= static_cast<SInt>(m);
+					}
+					return v;
+				}
+			}
+
+		template<
+			typename Flt, std::size_t Size=sizeof(Flt),
+			typename Byte=TStreamByte
+			>
+			auto ByteUnpackFloat(std::array<Byte, Size> arr) noexcept -> Flt {
+				static_assert(
+					std::is_floating_point_v<Flt> and (Size == 4 or Size == 8),
+					"BinON expects binary32 or binary64 floating-point types");
+				if(LittleEndian()) {
+					std::reverse(arr.begin(), arr.end());
+				}
+				if constexpr(Size == 4) {
+					types::TFloat32 x;
+					memcpy(&x, arr.data(), Size);
+					return static_cast<Flt>(x);
+				}
+				else {
+					types::TFloat64 x;
+					memcpy(&x, arr.data(), Size);
+					return static_cast<Flt>(x);
+				}
+			}
+	}
+
+	//	BytePack function
+	//
+	//	Packs a numeric value into an array of bytes.
+	//
+	//	Template args:
+	//		Size: number of bytes to pack or the default 0
+	//			In the 0 case, the number of bytes is determined from the size
+	//			of the type T. (This works best if you use a type with of well-
+	//			defined size such as std::int32_t.)
+	//
+	//			For integral types, if Size is set too small to accommodate the
+	//			entire value, the most-significant bytes will be discarded.
+	//
+	//			For floating-point types, the size must be either 4 or 8, since
+	//			BinON only supports binary32 and binary64 values.
+	//		Byte: a byte-like type (defaults to TStreamByte)
+	//			This is the element type of the returned std::array.
+	//		T (inferred from function arg "v"): type of the numeric value
+	//
+	//	Function args:
+	//		v: a numeric value
+	//
+	//	Returns:
+	//		a std::array of Byte elements containing the packed "v" value
+	//
+	template<std::size_t Size=0, typename Byte=TStreamByte, typename T>
+		auto BytePack(T v) noexcept -> std::array<Byte, kBytePackSize<T, Size>>
+		{
+			if constexpr(std::is_floating_point_v<T>) {
+				return details::BytePackFloat<Size, Byte, T>(v);
+			}
+			else {
+				return details::BytePackInt<Size, Byte, T>(v);
+			}
+		}
+
+	//	ByteUnpack
+	//
+	//	Unpack a numeric value packed into an array earlier by BytePack().
+	//
+	//	Template args:
+	//		T: type of the numeric value
+	//		Size (inferred from "arr" function arg): number of bytes to unpack
+	//		Byte (inferred from "arr" function arg): array element type
+	//			This should a byte-like type.
+	//
+	//	Function args:
+	//		arr: array of type Byte and size Size containing packed bytes
+	//
+	//	Returns:
+	//		value of type T unpacked from array
+	//
+	template<typename T, typename Byte, std::size_t Size>
+		auto ByteUnpack(const std::array<Byte, Size>& arr) noexcept -> T {
+			if constexpr(std::is_floating_point_v<T>) {
+				return details::ByteUnpackFloat<T, Size, Byte>(arr);
+			}
+			else if constexpr(std::is_unsigned_v<T>) {
+				return details::ByteUnpackUInt<T, Size, Byte>(arr);
+			}
+			else {
+				return details::ByteUnpackSInt<T, Size, Byte>(arr);
+			}
+		}
+
+	//	WriteAsBytes function
+	//
+	//	Calls BytePack to pack a numeric value into bytes before writing the
+	//	bytes to a binary output stream.
+	//
+	//	Template args:
+	//		Size: see PackBytes
+	//		T (inferred from "v" function arg): type the numeric value
+	//
+	//	Function args:
+	//		stream: a binary output stream
+	//		v: the value to stream out
+	//		requireIO: throw exception if write fails? (defaults to true)
+	//			Setting this false does not necessarily mean exceptions are
+	//			disabled. The stream will be accessed as-is in that case.
+	//
+	template<std::size_t Size=0, typename T>
+		void WriteAsBytes(TOStream& stream, T v, bool requireIO=true) {
+			auto arr = BytePack<Size>(v);
+			RequireIO rio(stream, requireIO);
+			stream.write(arr.data(), arr.size());
+		}
+
+	//	ReadAsBytes function
+	//
+	//	Calls ByteUnpack to unpack a numeric value from a byte sequence read
+	//	from a binary input stream.
+	//
+	//	Template args:
+	//		T: type of numeric value
+	//		Size: number of bytes to read and unpack (default to sizeof(T))
+	//
+	//	Function args:
+	//		stream: a binary input stream
+	//		requireIO: throw exception if read fails? (defaults to true)
+	//			Setting this false does not necessarily mean exceptions are
+	//			disabled. The stream will be accessed as-is in that case.
+	//
+	//			If exceptions ARE disabled, the return value may be corrupt.
+	//			You should check the iostate bits of the stream to determine
+	//			whether the value can be trusted?
+	//
+	//	Returns:
+	//		numeric value of type T read from stream and unpacked
+	//
+	template<typename T, std::size_t Size=sizeof(T)>
+		auto ReadAsBytes(TIStream& stream, bool requireIO=true) -> T {
+			std::array<TStreamByte, Size> arr;
+			RequireIO rio(stream, requireIO);
+			stream.read(arr.data(), arr.size());
+			return ByteUnpack<T, Size>(arr);
+		}
+
+	//-------------------------------------------------------------------------
+	//
+	//	Legacy Read/WriteWord functions
+	//
+	//-------------------------------------------------------------------------
 
 	//	WriteWord function template - byte buffer variant
 	//
